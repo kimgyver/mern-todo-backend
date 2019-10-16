d249125b4c8393968b68464658d94a15d0345a3b
unknown <jinyoung777@gmail.com>
2019-10-16 14:40:52 +1300

    heroku env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const PORT = 80;
 
 let Todo = require('./todo.model');
 
